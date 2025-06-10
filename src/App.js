3acea9cabe6844db1cd45bab7a19852fa28c5991
Intrepidpay <emekabright737@gmail.com>
2025-06-10 01:35:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import './App.css';
 function RouteTranslator() {
   const location = useLocation();
   const [prevPath, setPrevPath] = useState('');
- // eslint-disable-next-line
+ // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     // Only translate if path actually changed
     if (location.pathname !== prevPath) {
