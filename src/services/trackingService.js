fc36b82183422bb25f757dbd8436ce06152028a8
Intrepidpay <emekabright737@gmail.com>
2025-08-09 20:42:09 +0100

    Update trackingService.js

--- src/services/trackingService.js
+++ src/services/trackingService.js
@@ -38,7 +38,7 @@ let mockPackages = [
     recipient: "Satoru Ueno",
     destination: "4-13-3 Tatekawa Sumida-ku, Tokyo 130-0023 Japan",
     weight: "1.8 kg",
-    progress: 63,
+    progress: 71,
     createdAt: new Date("2025-08-07T21:26:00Z"),
     estimatedDelivery: new Date("2025-08-10T13:00:00Z"),
     history: [
