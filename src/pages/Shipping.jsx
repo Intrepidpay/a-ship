414be125ddc3a64c058553b14cbb2bc563100b86
Intrepidpay <emekabright737@gmail.com>
2025-08-11 00:29:48 +0100

    Update Shipping.jsx

--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -221,6 +221,13 @@ const PaymentPage = () => {
                       <span className="premium-detail-label">Method:</span>
                       <span className="premium-detail-value">{shippingInfo.method}</span>
                     </div>
+
+                    <div className="premium-detail-row">
+                      <span className="premium-detail-label">Sender:</span>
+                      <span className="premium-detail-value">{shippingInfo.sender}</span>
+                    </div>
+
+                    
                     <div className="premium-detail-row">
                       <span className="premium-detail-label">Tracking #:</span>
                       <span className="premium-detail-value">{shippingInfo.trackingId || 'N/A'}</span>
