5ad90010e02aa4c5775ae80579808c6a3bc2325e
Intrepidpay <emekabright737@gmail.com>
2025-09-21 17:02:07 +0100

    Update trackingService.js

--- src/services/trackingService.js
+++ src/services/trackingService.js
@@ -5,14 +5,14 @@ let mockPackages = [
     recipient: "Ryo Kuratomi",
     destination: "Apt 204 Gran Paseo/Primal, 2-17-46 Okubo, Shinjuku-ku, Tokyo 169-0072, Japan",
     weight: "800 g",
-    progress: 12,
+    progress: 18,
     createdAt: new Date("2025-09-20T23:53:00Z"),
     estimatedDelivery: new Date("2025-09-24T13:00:00Z"),
     history: [
       {
         timestamp: new Date("2025-09-21T20:17:00Z"),
         location: "Amman Warehouse",
-        status: "processing",
+        status: "processed",
         coordinates: { lat: 31.9539, lng: 35.9106 },
         isCurrentLocation: true
       }
